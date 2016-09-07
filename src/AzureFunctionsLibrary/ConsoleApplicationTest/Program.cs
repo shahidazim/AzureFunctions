75009f90353aa5707f10cfe8aac919862c789b0c
sazim <sazim@sunbrandingsolutions.com>
2016-09-07 12:35:33 +0100

    another possible fix :(

---
+++ src/AzureFunctionsLibrary/ConsoleApplicationTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using AzureFunctionsLibrary1;
+
+namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new Message
+            {
+                Content = "Hello World!"
+            };
+            var serializedMessage = SerializerJson.Serialize(message);
+            var deserializedMessage = SerializerJson.Deserialize(serializedMessage);
+        }
+    }
+}
